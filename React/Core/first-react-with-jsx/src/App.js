06bd05e76a79842fd8cb074476efd8b347280fbe
OmarHanchi <omarelhanchi@gmail.com>
2023-11-20 00:20:41 +0100

    first react app

---
+++ React/Core/first-react-with-jsx/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <h1>  hello dojo   </h1>
+      <h2>  things i need to do    </h2>
+      <h3>  learn react  </h3>
+      <h3>  climb mt everest   </h3>
+      <h3>  run a marathon   </h3>
+      <h3>  feed the dogs   </h3>
+    </div>
+  );
+}
+
+export default App;
