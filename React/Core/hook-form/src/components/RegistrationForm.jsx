ccb5c90dd3c1e757c864d33d5f7d33a497eabab6
OmarHanchi <omarelhanchi@gmail.com>
2023-11-27 00:25:27 +0100

    hook form assignement commit

---
+++ React/Core/hook-form/src/components/RegistrationForm.jsx
@@ -0,0 +1,63 @@
+import { Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+const RegistrationForm = (props) => {
+  const[firstName,setFirstName]=useState("");
+  const[lastName,setLastName]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[confirmPassword,setConfirmPassword]=useState("");
+  
+  const handleSubmit= (event) => {
+    event.preventDefault()
+    const newUser = {firstName,lastName,email,password,confirmPassword}
+    console.log("Welcome", newUser);
+    
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  return (
+    <div className="container">
+    <Form onSubmit={handleSubmit}>
+
+    <Form.Group className="mb-3" >
+        <Form.Label>First Name : </Form.Label>
+        <Form.Control type="text"  placeholder="Enter your first name"  value={firstName} onChange={ (e) => setFirstName(e.target.value) }/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label> Last Name : </Form.Label>
+        <Form.Control type="text" placeholder="Enter last  name" value={lastName} onChange={ (e) => setLastName(e.target.value)}/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label>Email address :</Form.Label>
+        <Form.Control type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label>Password :</Form.Label>
+        <Form.Control type="password" placeholder="Enter a Password" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" >
+        <Form.Label>Confirm Your Password : </Form.Label>
+        <Form.Control type="password" placeholder="Enter Password again " />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+
+    </Form>
+    </div>
+
+  );
+}
+
+export default RegistrationForm;
