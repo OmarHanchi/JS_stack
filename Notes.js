152d5e9a2bf1cdbb1d1bf8a31972f89a0636bf09
OmarHanchi <omarelhanchi@gmail.com>
2023-12-20 00:49:05 +0100

    mongo practice assignement commit

--- Notes.js
+++ Notes.js
@@ -182,3 +182,73 @@ app.delete("/api/users/:id", (req, res) => {
     res.json( { status: "ok" } );
 });
 
+
+
+---------------------Folder Structure
+
+server - will hold all server related files
+config - will handle the database configuration and connection
+controllers - will hold all logic for each model (i.e creating, updating, etc...)
+models - will hold all the schemas
+routes - will handle all of our routes for each model
+server.js - will handle all the server logic with express
+
+---------------------Modules
+
+To do this, we can use Javascript's built-in tools to import and export between files:
+
+require("./filePathGoesHere") - used to import from different files
+module.exports = WhateverYouNeedToExportGoesHere - used to export whatever content you need to export from that file
+
+
+
+--------------------------IMPORTANT NOTES:
+You'll notice that we require the string "./my_module". There are 2 things to note here:
+
+There is no .js at the end of the file. The require method automatically looks for JavaScript files, so we don't need to include the .js file extension.
+The files app.js and my_module.js are in the same directory. Normally, the require() method looks for node modules that aren't in the same directory as the file that is running; by default, the require() method looks for the modules located in a folder called node_modules. To tell require() to look in the current directory (i.e. the folder that the file is located in) we have to include "./" in front of the file path. "./" (dot-slash) is the file path for the current directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------- mongo db ------------------------------
+--------- CREATE - Inserting a document into a collection:
+// Pattern:
+db.COLLECTION_NAME.insert({YOUR_JSON_DOCUMENT})  
+ 
+// Example:
+db.ninjas.insert({name: "Trey", belt: "black", status: "awesome"})
+
+--------- READ - Retrieving documents from a collection:
+// Pattern: 
+db.COLLECTION_NAME.find({YOUR_QUERY_DOCUMENT})
+
+// Example:
+db.ninjas.find({name: "Trey"})
+
+---------- DESTROY - Removing documents from a collection:
+
+// Pattern:
+db.COLLECTION_NAME.remove({YOUR_QUERY_DOCUMENT}, BOOLEAN)
+// Example
+db.ninjas.remove({belt: "yellow"})
+db.ninjas.remove({belt: "yellow"}, false) // this query would have the same effect as the one above.
+
+---------UPDATE - Updating documents in a collection:
+// Example:
+db.ninjas.update({name: "Trey"}, {$set: {location: "Mountain View"}})
+
+//...............
+If I wanted to get all the Dojos whose number of students is greater than 15, I would run the following:
+
+db.dojos.find({number_of_students: {$gt: 15}})
